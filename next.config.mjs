a3ed2bbd77e09348b01994f3e6e8e55f4e2ae60e
Kostiantyn Yemets <61251118+kyemets@users.noreply.github.com>
2024-02-22 17:53:08 +0100

    added map component

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	reactStrictMode: true,
+	reactStrictMode: false,
 	env: {
 		MAP_KEY: process.env.REACT_APP_MAP_KEY,
 	}
--- src/app/components/screen/home/Map.tsx
+++ src/app/components/screen/home/Map.tsx
@@ -1,3 +1,4 @@
+"use client"
 import GoogleMapReact from 'google-map-react'
 
 
@@ -6,6 +7,12 @@ const Map = () => {
 	<div className='h-screen w-full'>
 		<GoogleMapReact
 			bootstrapURLKeys={{ key: String(process.env.REACT_APP_MAP_KEY) || ''}}
+			defaultCenter={{
+				lat: 50.1109221,
+				lng: 8.6821267
+			}}
+			defaultZoom={ 13 }
+			options={{}}
 		/>
 	</div>
   )
